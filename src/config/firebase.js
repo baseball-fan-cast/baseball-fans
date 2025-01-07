1a5452cf0851dc4cdf2b6f10cf8bc5cada192f0d
KateBabii <kateryna.babii@eleks.com>
2025-01-07 15:12:22 -0600

    Integrated firebase with auth page - update firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyAeb7KOoTCT47GoU1uSjjm_711bl1DgxME',
+  authDomain: 'fan-cast-api.firebaseapp.com',
+  projectId: 'fan-cast-api',
+  storageBucket: 'fan-cast-api.firebasestorage.app',
+  messagingSenderId: '149788204407',
+  appId: '1:149788204407:web:b1701ed5d4dd85c0169572',
+  measurementId: 'G-SWYFXJ0QLH'
 };
 
 // Initialize Firebase
