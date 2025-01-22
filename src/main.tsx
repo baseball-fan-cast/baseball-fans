81877627bdf258f1888068a94db502cd81b67e06
KateBabii <kateryna.babii@eleks.com>
2025-01-22 10:28:52 -0600

    revert StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -6,4 +6,8 @@ import './i18n';
 import '@radix-ui/themes/styles.css';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
