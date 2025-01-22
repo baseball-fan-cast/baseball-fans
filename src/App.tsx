6f0c385125075cc6e43aaef22f3e88a7794f243f
KateBabii <kateryna.babii@eleks.com>
2025-01-22 09:05:15 -0600

    added language to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Home } from './pages/Home';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ContentContextProvider } from './context/ContentContextProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider, AuthContext } from './context/AuthContext';
@@ -7,15 +7,26 @@ import { Routes as Router, Route, Navigate, Outlet } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { FollowFans } from './pages/FollowFans';
+import { useTranslation } from 'react-i18next';
 
 const PrivateRoutes = () => {
   const { token } = useContext(AuthContext);
+
   if (!token) return <Navigate to="/login" replace />;
 
   return <Outlet />;
 };
 
 const App = () => {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    const storedLanguage = localStorage.getItem('LANG');
+    if (storedLanguage) {
+      i18n.changeLanguage(storedLanguage);
+    }
+  }, [i18n]);
+
   return (
     <BrowserRouter>
       <AuthProvider>
