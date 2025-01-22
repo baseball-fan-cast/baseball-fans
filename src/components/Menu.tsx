8de27491eed050043bd1469acddfb04323584d68
KateBabii <kateryna.babii@eleks.com>
2025-01-22 10:30:38 -0600

    remove unsued code

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,29 +3,13 @@ import React, { useContext, useState, useEffect } from 'react';
 import { Flex, Text, Box } from '@radix-ui/themes';
 import { AvatarBadge } from '../components/AvatarBadge';
 import { CustomSearch } from '../components/CustomSearch';
-import { signOut } from 'firebase/auth';
-import { auth } from '../config/firebase';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext';
 import { ContentContext } from '@/context/ContentContextProvider';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const Menu = ({ subscriptions }: { subscriptions: any }) => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
-  const { setToken } = useContext(AuthContext);
   const { selectedFollower, setSelectedFollower } = useContext(ContentContext);
   const [followers, setFollowers] = useState([]);
-
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth);
-      setToken('');
-      navigate('/login');
-    } catch (error) {
-      console.error('Error signing out:', error);
-    }
-  };
 
   const getIcon = (currentTeam, id, isPlayer) => {
     const teamIcon = isPlayer
@@ -60,20 +44,6 @@ export const Menu = ({ subscriptions }: { subscriptions: any }) => {
               {t('viewAll')}
             </Text>
           </button>
-          {/* {followers?.map(({ name, icon, playerIcon, abbreviation, ...rest }) => (
-            <AvatarBadge
-              key={name}
-              content={name}
-              isSelected={selectedFollower?.name == name}
-              onSelect={() =>
-                setSelectedFollower({ name, icon, playerIcon, abbreviation, ...rest })
-              }
-              data={[
-                { src: icon, fallback: abbreviation },
-                ...(playerIcon ? [{ src: playerIcon, fallback: abbreviation }] : [])
-              ]}
-            />
-          ))} */}
           {followers?.map(({ name, abbreviation, teamName, currentTeam, id }) => {
             const { icon, playerIcon } = getIcon(currentTeam, id, !teamName) || {};
 
@@ -93,11 +63,6 @@ export const Menu = ({ subscriptions }: { subscriptions: any }) => {
         </Flex>
         <Box className="w-[300px] justify-between items-center flex-1">
           <CustomSearch />
-        </Box>
-        <Box>
-          <button className="bg-blue-700 text-white rounded-xl py-2 px-4" onClick={handleSignOut}>
-            Log out
-          </button>
         </Box>
       </Flex>
     </Box>
